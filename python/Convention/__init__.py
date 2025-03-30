814da10bc0818f09be797f27dea948bd2eb08823
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2025-03-30 13:25:21 +0800

    Update __init__.py
    取消全体引用

--- python/Convention/__init__.py
+++ python/Convention/__init__.py
@@ -4,19 +4,19 @@ Convention - A comprehensive Python utility library
 
 __version__ = "0.2.2"
 
-from . import Algorithm
-from . import Audio
-from . import DataBase
-from . import Extensions
-from . import File
-from . import Internal
-from . import Lang
-from . import LLM
-from . import Str
-from . import ML
-from . import MathEx
-from . import Lang
-from . import Str
-from . import UI
-from . import Visual
-from . import Web
+#from . import Algorithm
+#from . import Audio
+#from . import DataBase
+#from . import Extensions
+#from . import File
+#from . import Internal
+#from . import Lang
+#from . import LLM
+#from . import Str
+#from . import ML
+#from . import MathEx
+#from . import Lang
+#from . import Str
+#from . import UI
+#from . import Visual
+#from . import Web
