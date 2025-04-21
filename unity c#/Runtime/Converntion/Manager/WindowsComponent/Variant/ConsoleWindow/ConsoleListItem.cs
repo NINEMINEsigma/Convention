29be6d40c7f92e59b6ee7a0db36e56e2eb8e12c7
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2025-04-21 22:47:59 +0800

    开始推进Console Component

--- unity c#/Runtime/Converntion/Manager/WindowsComponent/Variant/ConsoleWindow/ConsoleListItem.cs
+++ unity c#/Runtime/Converntion/Manager/WindowsComponent/Variant/ConsoleWindow/ConsoleListItem.cs
@@ -1,25 +1,42 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Convention.WindowsUI.Variant;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Convention.WindowsUI.Variant
 {
-    public class ConsoleListItem : PropertyListItem
+    public class ConsoleListItem : WindowUIModule,IText,ITitle
     {
+        [Resources, SerializeField, OnlyNotNullMode] private Text MyTitleText;
+        [Resources, SerializeField, OnlyNotNullMode] private Button RawButton;
+
         public string stackTrace;
         public bool IsEnableFocusWindow;
+        public LogType logType;
 
+        public string text { get => ((IText)this.MyTitleText).text; set => ((IText)this.MyTitleText).text = value; }
+        public string title { get => ((ITitle)this.MyTitleText).title; set => ((ITitle)this.MyTitleText).title = value; }
+
+        public void SetupMessage(string message, string stackTrace, string color, LogType logType, string format = "<color={color}>{message}</color>")
+        {
+            format = format.Replace("color", color);
+            format = format.Replace("message", message);
+            this.title = format;
+            this.stackTrace = stackTrace;
+            this.logType = logType;
+        }
+
+        protected void Start()
+        {
+            RawButton.onClick.AddListener(OnFocusConsoleItem);
+        }
 
         [Content]
-        public void OnFocusHierarchyItem()
+        public void OnFocusConsoleItem()
         {
+            ConsoleWindow.instance.SetStackTrace(this.stackTrace);
             if (!IsEnableFocusWindow)
                 return;
             if (FocusWindowIndictaor.instance != null)
-                FocusWindowIndictaor.instance.SetTargetRectTransform(TextRectTransform);
+                FocusWindowIndictaor.instance.SetTargetRectTransform(MyTitleText.transform as RectTransform);
         }
-
     }
 }
--- unity c#/Runtime/Converntion/Manager/WindowsComponent/Variant/ConsoleWindow/ConsoleWindow.cs
+++ unity c#/Runtime/Converntion/Manager/WindowsComponent/Variant/ConsoleWindow/ConsoleWindow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace Convention.WindowsUI.Variant
@@ -11,10 +12,15 @@ namespace Convention.WindowsUI.Variant
         [Resources, SerializeField, OnlyNotNullMode, Header("Bar Button Setting")] private WindowManager m_WindowManager;
         [Resources, SerializeField, OnlyNotNullMode(nameof(m_WindowManager))] private RectTransform m_root;
         [Resources, SerializeField, OnlyNotNullMode(nameof(m_WindowManager))] private RectTransform m_plane;
-        [Content, SerializeField, OnlyPlayMode, Ignore, OnlyNotNullMode(nameof(m_WindowManager))] private int m_registeredIndex;
-        public BaseWindowBar.RegisteredPageWrapper ConsoleWindowIndex { get; private set; }
+        public int ConsoleWindowIndex { get; private set; }
         [Header("Property Window - ListView"), Resources, SerializeField, OnlyNotNullMode] private PropertiesWindow m_ListView;
         private List<PropertiesWindow.ItemEntry> m_entries = new();
+        [Resources, SerializeField, OnlyNotNullMode] private ModernUIInputField StackTrace;
+
+        [Header("Message Switch"), Resources, SerializeField, OnlyNotNullMode] private ModernUIToggle m_MessageSwitch;
+        [Resources, SerializeField, OnlyNotNullMode] private ModernUIToggle m_WarningSwitch;
+        [Resources, SerializeField, OnlyNotNullMode] private ModernUIToggle m_VitalSwitch;
+        [Resources, SerializeField, OnlyNotNullMode] private Button m_ClearLogs;
 
         public void ClearLog()
         {
@@ -27,20 +33,100 @@ namespace Convention.WindowsUI.Variant
 
         public void Log(string condition, string stackTrace, LogType type = LogType.Log)
         {
-            PropertiesWindow.ItemEntry entry = m_ListView.CreateRootItemEntries(1)[0];
-            m_entries.Add(entry);
-            //entry.ref_value.GetComponent<ConsoleListItem>()
+            ConsoleListItem item;
+            string color;
+            GenerateLogItem(type, out item, out color);
+            item.SetupMessage(condition, stackTrace, color, type);
+        }
 
+        private void GenerateLogItem(LogType type, out ConsoleListItem item, out string color)
+        {
+            bool isActive = type switch
+            {
+                LogType.Log => m_MessageSwitch.ref_value,
+                LogType.Warning => m_WarningSwitch.ref_value,
+                _ => m_VitalSwitch.ref_value
+            };
+            PropertiesWindow.ItemEntry entry = m_ListView.CreateRootItemEntries(isActive, 1)[0];
+            m_entries.Add(entry);
+            item = entry.ref_value.GetComponent<ConsoleListItem>();
+            color = type switch
+            {
+                LogType.Log => "white",
+                LogType.Warning => "yellow",
+                _ => "red"
+            };
+        }
+
+        public void Log(string condition, string stackTrace, LogType type, string format)
+        {
+            ConsoleListItem item;
+            string color;
+            GenerateLogItem(type, out item, out color);
+            item.SetupMessage(condition, stackTrace, color, type, format);
+        }
+
+        public void SetStackTrace(string str)
+        {
+            StackTrace.text = str;
         }
 
         private void Start()
         {
             Application.logMessageReceived -= Log;
             Application.logMessageReceived += Log;
-            ConsoleWindowIndex = m_WindowManager.CreateSubWindowWithBarButton(m_plane, m_root);
-            ConsoleWindowIndex.button.AddListener(() =>
+            ConsoleWindowIndex = m_WindowManager.AddContextPlane(m_plane, m_root);
+            var buttonWrapper = m_WindowManager.CreateWindowBarButton(() =>
             {
-                ConsoleWindowIndex.Select();
+                m_WindowManager.SelectContextPlane(ConsoleWindowIndex);
+            });
+            (buttonWrapper.button as ITitle).title = "Console";
+            StackTrace.interactable = false;
+
+            m_MessageSwitch.ref_value = true;
+            m_WarningSwitch.ref_value = true;
+            m_VitalSwitch.ref_value = true;
+
+            m_MessageSwitch.AddListener(x =>
+            {
+                foreach (var entry in m_entries)
+                {
+                    var item = entry.ref_value.GetComponent<ConsoleListItem>();
+                    if (item.logType == LogType.Log)
+                    {
+                        item.gameObject.SetActive(x);
+                    }
+                }
+            });
+            m_WarningSwitch.AddListener(x =>
+            {
+                foreach (var entry in m_entries)
+                {
+                    var item = entry.ref_value.GetComponent<ConsoleListItem>();
+                    if (item.logType == LogType.Warning)
+                    {
+                        item.gameObject.SetActive(x);
+                    }
+                }
+            });
+            m_VitalSwitch.AddListener(x =>
+            {
+                foreach (var entry in m_entries)
+                {
+                    var item = entry.ref_value.GetComponent<ConsoleListItem>();
+                    if (item.logType != LogType.Log && item.logType != LogType.Warning)
+                    {
+                        item.gameObject.SetActive(x);
+                    }
+                }
+            });
+            m_ClearLogs.onClick.AddListener(() =>
+            {
+                foreach (var entry in m_entries)
+                {
+                    entry.Release();
+                }
+                m_entries.Clear();
             });
         }
 
