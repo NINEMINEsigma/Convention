880a8df7ef5db0f6b7c57b40959a96fc08f4fabf
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2025-05-27 14:11:07 +0800

    Update ModuleInterfaces.cs
    修复一些错误

--- unity c#/Runtime/Converntion/Manager/WindowsComponent/ModuleInterfaces.cs
+++ unity c#/Runtime/Converntion/Manager/WindowsComponent/ModuleInterfaces.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Convention.WindowsUI
@@ -9,16 +10,47 @@ namespace Convention.WindowsUI
 
     public static partial class UIInterfaceExtension
     {
+        public static void AutoTransform(GameObject @object)
+        {
+            if (@object == null) return;
+            foreach (var text in @object.GetComponentsInChildren<IText>(true))
+            {
+                text.Transform();
+            }
+            foreach (var title in @object.GetComponentsInChildren<ITitle>(true))
+            {
+                title.Transform();
+            }
+            foreach (Transform child in @object.transform)
+            {
+                AutoTransform(child.gameObject);
+            }
+        }
+
         public static void Transform(this IText self)
         {
             try
             {
                 string result = self.text;
-                foreach (Match match in Regex.Matches(self.text, @"$$.*?$$"))
+                foreach (Match match in Regex.Matches(self.text, @"\$\$.*?\$\$"))
                 {
                     result = result.Replace(match.Value, StringExtension.Transform(match.Value[2..^2]));
                 }
                 self.text = result;
+            }
+            catch (System.Exception) { }
+        }
+
+        public static void Transform(this ITitle self)
+        {
+            try
+            {
+                string result = self.title;
+                foreach (Match match in Regex.Matches(self.title, @"\$\$.*?\$\$"))
+                {
+                    result = result.Replace(match.Value, StringExtension.Transform(match.Value[2..^2]));
+                }
+                self.title = result;
             }
             catch (System.Exception) { }
         }
