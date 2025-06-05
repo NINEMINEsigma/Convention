6191d109e58f94710024893f79fbf7841aa1952e
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2025-06-05 22:49:36 +0800

    Update TreeInstance.h

--- cpp/Convention/instance/TreeInstance.h
+++ cpp/Convention/instance/TreeInstance.h
@@ -5,25 +5,94 @@
 
 namespace Convention
 {
-	template<typename Element, template<typename> class Allocator>
+	template<typename Element, bool EnableParent = true, template<typename> class Allocator = std::allocator>
 	class TreeNode
 	{
-	private:
-		UniquePtr<TreeNode, DefaultDelete<TreeNode, Allocator>> parent, next, child;
 	public:
+		using _InsidePtr = SharedPtr<TreeNode>;//UniquePtr<TreeNode, DefaultDelete<TreeNode, Allocator>>;
+		using _SharedOutsidePtr = WeakPtr<TreeNode>;
+		using _PtrContainer = std::conditional_t<EnableParent, std::tuple<_InsidePtr, _InsidePtr, _InsidePtr>, std::tuple<_InsidePtr, _InsidePtr>>;
+		constexpr static size_t _ParentOffset = 0;
+		constexpr static size_t _NextOffset = 1 - (EnableParent ? 1 : 0);
+		constexpr static size_t _ChildOffset = 2 - (EnableParent ? 1 : 0);
+	private:
+		_PtrContainer container;
+
+		static SetNodeLayerParent(_InsidePtr oldParent, _InsidePtr newParent)
+		{
+
+		}
+
+	public:
+		_InsidePtr ReadParentNode() const noexcept
+		{
+			if constexpr (EnableParent)
+				return std::get<_ParentOffset>(container);
+			else
+				throw std::logic_error("TreeNode does not support parent node access when EnableParent is false.");
+		}
+		_InsidePtr ReadNextNode() const noexcept
+		{
+			return std::get<_NextOffset>(container);
+		}
+		_InsidePtr ReadChildNode() const noexcept
+		{
+			return std::get<_ChildOffset>(container);
+		}
+
 		Element data;
 
+		/**
+		* @brief 是否有父节点
+		*/
 		bool HasParent() const noexcept
 		{
-			return parent != nullptr;
+			if constexpr (EnableParent)
+				return ReadParentNode() != nullptr;
+			else
+				return false;
 		}
+		/**
+		* @brief 是否是根节点(无父节点且无兄弟节点)
+		*/
 		bool IsRoot() const noexcept
 		{
-			return !HasParent();
+			return !HasParent() && !HasNext();
+		}
+		/**
+		* @brief 获取父节点
+		*/
+		_SharedOutsidePtr GetParent() const noexcept
+		{
+			if constexpr (EnableParent)
+				return ReadParentNode();
+			else
+				return nullptr;
+		}
+		/**
+		* @brief 设置父节点
+		* @return 返回原父节点
+		*/
+		_InsidePtr SetParent(_InsidePtr&& newParent)
+		{
+			if (IsRoot())
+			{
+				_InsidePtr oldParent = std::move(parent);
+				parent = std::move(newParent);
+				return std::move(oldParent);
+			}
+			else if(HasParent())
+			{
+
+			}
 		}
 		bool HasChild() const noexcept
 		{
 			return child != nullptr;
+		}
+		TreeNode& GetChild() const noexcept
+		{
+			return *child;
 		}
 		bool IsLeaf() const noexcept
 		{
@@ -37,19 +106,11 @@ namespace Convention
 		{
 			return !HasNext();
 		}
-
-		TreeNode& GetParent() const noexcept
-		{
-			return *parent;
-		}
-		TreeNode& GetChild() const noexcept
-		{
-			return *child;
-		}
 		TreeNode& GetNext() const noexcept
 		{
 			return *next;
 		}
+
 	};
 }
 
