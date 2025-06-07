49cb02b657821bb6b434aabc70639c236d711328
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2025-06-07 15:11:14 +0800

    将使用适配器作为Tree的实现方式

--- cpp/Convention/instance/TreeInstance.h
+++ cpp/Convention/instance/TreeInstance.h
@@ -5,68 +5,21 @@
 
 namespace Convention
 {
-	struct TreeIndictor
+	struct TreeIndicator
 	{
 		template<
 			typename Element,
-			bool Enable2EndOperator,
-			template<typename> class Allocator = std::allocator
+			bool EnableParent,
+			bool EnableBranchLayer,
+			template<typename> class TreeAllocator,
+			template<typename> class Allocator
 		>
-		struct ListTrait
+		struct BidirectionalListsTrait
 		{
-
-
-			using tag = Element;
-			using Tree = TreeNode <Element, Enable2EndOperator, false, Allocator>;
-			using Iterator = TreeNodeIterator<Element, Enable2EndOperator, false, Allocator, Tree::ChildOffset>;
-			using ReverseIterator = TreeNodeIterator<Element, Enable2EndOperator, false, Allocator, Tree::ParentOffset>;
+			using ListNode = TreeNode<Element, true, false, TreeAllocator>;
+			using ListNodeIterator = TreeNodeIterator<Element, true, false, TreeAllocator, ListNode::ChildOffset>;
+			using ListNodeReverseIterator = TreeNodeIterator<Element, true, false, TreeAllocator, ListNode::ParentOffset>;
 		};
-	};
-
-	template<typename Element, template<typename> class TreeAllocator, template<typename> class Allocator>
-	class instance<TreeNode<Element, false, false, TreeAllocator>, true, Allocator, false>
-		: public instance<TreeNode<Element, false, false, TreeAllocator>, false, Allocator, false>
-	{
-	public:
-		using Trait = TreeIndictor::ListTrait<Element, false, Allocator>;
-		using Tree = Trait::Tree;
-		using Iterator = Trait::Iterator;
-	private:
-		using _Mybase = instance<Tree, false, Allocator, false>;
-		SharedPtr<Tree> tail;
-	public:
-		Iterator begin() noexcept
-		{
-			return Iterator(*this);
-		}
-		constexpr Iterator end() noexcept
-		{
-			return Iterator(nullptr);
-		}
-		Iterator cbegin() noexcept
-		{
-			return Iterator(*this);
-		}
-		constexpr Iterator cend() noexcept
-		{
-			return Iterator(nullptr);
-		}
-
-		template<typename Arg, std::enable_if_t<std::is_convertible_v<Arg, Element>, size_t> = 0>
-		void push(Arg&& value)
-		{
-			SharedPtr<Tree> temp(BuildMyPtr());
-			temp->container.SetValue<Tree::ElementOffset>(std::forward<Arg>(value));
-			if (this->IsEmpty())
-			{
-				this->WriteValue(std::move(temp));
-				tail = *this;
-			}
-			else
-			{
-				tail->container.SetValue
-			}
-		}
 	};
 }
 
